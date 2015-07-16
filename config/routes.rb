227193d1b5e1136fb01b467c55390818dbb5c873
ZuriHunter <zurihunter92@gmail.com>
2015-07-16 00:29:16 +0000

    Setup Reviews Component

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  
+
   devise_for :users
-  resources :movies
+  
+  resources :movies do
+    resources :reviews, except: [:show, :index]
+  end 
+  #allows for nesting routes within example movie/movie_id/review 
   
   root 'movies#index'
 
